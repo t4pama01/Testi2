dd9cebbf3e933ef825ed1612f6963883d0879d41
t4pama01@students.oamk.fi <t4pama01@students.oamk.fi>
2016-11-09 13:58:20 +0200

    Viimeistely tehty

--- application/controllers/asiakas.php
+++ application/controllers/asiakas.php
@@ -3,11 +3,57 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class asiakas extends CI_Controller {
 
+	function __construct() {
+        parent::__construct();
+        $this->load->model('Asiakas_model');
+    }
+
 	public function naytaAsiakas() {
-		$this->load->model('Asiakas_model');
+		
 		$data['asiakkaat']=$this->Asiakas_model->getAsiakas();
+		
 		$data['sivun_sisalto']="asiakas/naytaAsiakas";
 		$this->load->view('menu/valikko',$data);
+	}
+
+	public function lisaaAsiakas() {
+		$btn=$this->input->post('btn');
+		//Jos painiketta painettu
+		if(isset($btn)) {
+			$lisaa_data=array(
+				"etunimi"=>$this->input->post('en'),
+				"sukunimi"=>$this->input->post('sn'),
+				"email"=>$this->input->post('email')
+				);
+			$testi=$this->Asiakas_model->addAsiakas($lisaa_data);
+			if($testi>0) {
+				$data['ilmoitus']="Asiakkaan lisäys onnistui";
+			}
+		}
+		$data['sivun_sisalto']="asiakas/lisaaAsiakas";
+		$this->load->view('menu/valikko',$data);
+	}
+
+	public function naytaMuokattava($id) {
+		$data['asiakas'] = $this->Asiakas_model->getValittuAsiakas($id);
+		$data['sivun_sisalto']="asiakas/naytaMuokattava";
+		$this->load->view('menu/valikko',$data);
+	}
+
+	public function muokkaaAsiakas() {
+		$btn=$this->input->post('btn');
+		$update_id=$this->input->post('id');
+		if(isset($btn)) {
+			$update_data = array(
+				"etunimi"=>$this->input->post('en'),
+				"sukunimi"=>$this->input->post('sn'),
+				"email"=>$this->input->post('email')	
+				);
+			$testi=$this->Asiakas_model->updateAsiakas($update_id,$update_data);
+			if($testi>0) {
+				$this->naytaAsiakas();
+			}
+		}
 	}
 
 }
--- application/views/menu/header.php
+++ application/views/menu/header.php
@@ -6,5 +6,7 @@
 <body>
 
 <ul>
-<li>Asiakastiedot</li>
+<li><a href="<?php echo site_url('home'); ?>">Pääsivu</a></li>
+<li><a href="<?php echo site_url('asiakas/naytaAsiakas'); ?>">Asiakastiedot</a></li>
+<li><a href="<?php echo site_url('asiakas/lisaaAsiakas'); ?>">Lisää asiakas</a></li>
 </ul>
